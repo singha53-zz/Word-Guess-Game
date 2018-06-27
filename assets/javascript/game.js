382b172e9ea38cf191abde1845f3490dfed28e34
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-06-26 22:22:06 -0400

    declare variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log('DOM fully loaded and parsed');
+
+  // 1. declare variables
+  var wins = 0;
+  var losses = 0;
+  var tries = 9;
+  var triesLeft = 9;
+  var guesses = [];
+});
