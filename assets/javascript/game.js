243e683891255d2496a35dbf9b57dfd66f1981a9
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-06-26 22:24:55 -0400

    add list of FIFA countries

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,4 +14,9 @@ document.addEventListener('DOMContentLoaded', function(event) {
   document.getElementById('country').style.display = 'flex';
   document.getElementById('country').style.alignItems = 'space-between';
   document.getElementById('country').style.justifyContent = 'center';
+
+  // 3. list of countries the user can guess
+  countries = ['Argentina', 'Brazil', 'Morocco', 'Mexico'].map(x =>
+    x.toUpperCase()
+  );
 });
