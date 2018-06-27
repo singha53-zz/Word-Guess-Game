ddd425262d74b482488841798c5e1b790d9eee20
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-06-26 22:23:22 -0400

    add css styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,4 +7,11 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var tries = 9;
   var triesLeft = 9;
   var guesses = [];
+
+  // 2. set CSS styles
+  document.getElementById('hangman').style.alignItems = 'space-between';
+  document.getElementById('country').style.padding = 0;
+  document.getElementById('country').style.display = 'flex';
+  document.getElementById('country').style.alignItems = 'space-between';
+  document.getElementById('country').style.justifyContent = 'center';
 });
